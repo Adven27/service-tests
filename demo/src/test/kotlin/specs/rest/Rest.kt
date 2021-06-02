b4af96e8a9b6cefa1ba537a91a561251b0db5d50
MetallFoX <metallfox@list.ru>
2021-06-02 17:41:41 +0300

    fix typo

--- demo/src/test/kotlin/specs/rest/Rest.kt
+++ demo/src/test/kotlin/specs/rest/Rest.kt
@@ -8,7 +8,7 @@ class RestDeposit : Specs() {
     fun resetSesInteractions() = ENV.ses().resetRequests()
     fun sesInteractionsCount() = interactions().count()
     fun sesNoInteractions() = interactions().count() == 0
-    fun getSesResponse(): String = interactions()[0]
+    fun getSesRequest(): String = interactions()[0]
 
     private fun interactions() = ENV.ses().find(postRequestedFor(urlPathEqualTo("/hook"))).map { it.bodyAsString }
 }
